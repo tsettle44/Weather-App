bdda5dbca465690deae31e5fba2cc45c20e32be3
tsettle44 <tsettle44@gmail.com>
2018-09-11 11:11:48 -0400

    data.json test file

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
 function search () {
-    var cityName = document.getElementById('city').value
-    var stateName = document.getElementById('state').value
+
     var zipCode = document.getElementById('zipCode').value
 
-    console.log(cityName, stateName, zipCode)
+    console.log(zipCode)
 
     var request = new XMLHttpRequest();
 
+    /*
     request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip='+zipCode+',us&appid=26e9a3f1568c93129c774adfb8c5aecd', true);
     request.onload = function () {
 
     // Begin accessing JSON data here
     var data = JSON.parse(this.response);
 
-    console.log(data)
-    }
-
     request.send();
+    }
+    */
+
+    var data = [
+        {
+            "coord": {
+            "lon": -122.09,
+            "lat": 37.39
+            },
+            "weather": [
+            {
+            "id": 500,
+            "main": "Rain",
+            "description": "light rain",
+            "icon": "10d"
+            }
+            ],
+            "base": "stations",
+            "main": {
+            "temp": 280.44,
+            "pressure": 1017,
+            "humidity": 61,
+            "temp_min": 279.15,
+            "temp_max": 281.15
+            },
+            "visibility": 12874,
+            "wind": {
+            "speed": 8.2,
+            "deg": 340,
+            "gust": 11.3
+            },
+            "clouds": {
+            "all": 1
+            },
+            "dt": 1519061700,
+            "sys": {
+            "type": 1,
+            "id": 392,
+            "message": 0.0027,
+            "country": "US",
+            "sunrise": 1519051894,
+            "sunset": 1519091585
+            },
+            "id": 0,
+            "name": "Mountain View",
+            "cod": 200
+        }
+    ]
+
+    console.log(data[0].name)
+        
 }
